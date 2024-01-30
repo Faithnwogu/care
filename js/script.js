e9f32174e78b09a55aea36036d25de72a4011db9
Faithnwogu <dfaytech@gmail.com>
2024-01-30 10:46:27 +0100

    first commit for care

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// mobile nav
+
+const mobileLinks = document.getElementById('mobileLinks');
+const mobileIcon = document.getElementById('mobileicon');
+
+
+mobileIcon.addEventListener('click', ()=>{
+       mobileLinks.classList.toggle('toggle');
+})
+
+
+// display department information
